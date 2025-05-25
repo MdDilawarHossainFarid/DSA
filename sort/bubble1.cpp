e06060e8380b9e7caa2a2403fbc7959b48d85501
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2025-05-25 17:45:03 +0530

    bubbleSort

---
+++ sort/bubble1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void bubbleSort(int arr[],int n){
+    int isSwap;
+    for(int i = 0; i < n - 1; i++){
+   
+        for(int j = 0; j < n - 1; j++){
+            if(arr[j] > arr[j + 1]){
+                swap(arr[j],arr[j +1]);
+              
+            }
+        }
+      
+    }
+
+    for(int i = 0; i < n; i++){
+        cout << arr[i];
+    }
+
+}
+
+int main(){
+    int arr[] = {5,4,3,2,1};
+    int size =  sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr,size);
+}
