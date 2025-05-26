df68b34a26c69afc290b72c59628d6af6dab0bb2
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2025-05-26 21:08:31 +0530

    access array elements in reverse order

---
+++ arrayBasic/reverse.cpp
@@ -0,0 +1,16 @@
+
+//  How do you access array elements in reverse order?
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int arr[] = {1,2,3,4,5,6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+
+    for(int i = n - 1; i >= 0; i--){
+        cout << i << "->" <<arr[i] << endl;
+    }
+}
