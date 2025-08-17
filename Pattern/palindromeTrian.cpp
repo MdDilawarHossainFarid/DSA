c3cd9d70bc05a01b8c52840e6ed005a0358ca543
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2025-08-17 14:45:34 +0530

    palindromeTrian

--- Pattern/palindromeTrian.cpp
+++ Pattern/palindromeTrian.cpp
@@ -5,9 +5,18 @@ using namespace std;
 
 int main(){
 for(int i = 1; i <= 4; i++){
-    for(int j = i; j>= 1; j--){
-        cout << j <<" ";
+
+    for(int a = 1; a <= 4 - i; a++){
+        cout << " " <<" ";
     }
+    
+    for(int j = i; j>= 1; j--){
+        cout << j << " ";
+    }
+    for(int k = 2; k <= i; k++){
+        cout << k << " ";
+    }
+
     cout << endl;
 }
 }
